ad64485bc10a22291e5043b437a55682460fd146
adamcanbuildit <adam.inthemorning@gmail.com>
2022-02-28 08:22:06 -0500

    initial file upload

---
+++ src/main/java/com/dojogroup/pizzatime/PizzatimeApplication.java
@@ -0,0 +1,13 @@
+package com.dojogroup.pizzatime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzatimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzatimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dojogroup/pizzatime/PizzatimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojogroup.pizzatime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzatimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
